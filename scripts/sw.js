a3e3c89818023cdfc68ced197bfa2fb15c162233
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 <17191170703@utectulancingo.edu.mx>
2023-03-04 15:10:27 -0600

    CV v1.2

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,23 +1,39 @@
-const staticMoneda = "dev-cv-v1"
-const assets = [
-    "/pwa-cv/docs/",
-    "/pwa-cv/images/",
-    "/pwa-cv/index.html",
-    "/pwa-cv/scripts/",
-    "/pwa-cv/manifest.json",
-]
+const CACHE_NAME = `dev-cv-v1`;
 
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(staticMoneda).then(cache => {
-            cache.addAll(assets)
-        })
-    )
-})
-self.addEventListener("fetch", fetchEvent =>{
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
-        })
-    )
-})
+// Use the install event to pre-cache all initial resources.
+self.addEventListener('install', event => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll([
+        "/pwa-cv/docs/",
+        "/pwa-cv/images/",
+        "/pwa-cv/index.html",
+        "/pwa-cv/scripts/",
+        "/pwa-cv/manifest.json"
+    ]);
+  })());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith((async () => {
+    const cache = await caches.open(CACHE_NAME);
+
+    // Get the resource from the cache.
+    // If it's not in the cache, get it from the network.
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+      return cachedResponse;
+    } else {
+        try {
+          // If the resource was not in the cache, try the network.
+          const fetchResponse = await fetch(event.request);
+
+          // Save the resource in the cache and return it.
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        } catch (e) {
+          // The network failed.
+        }
+    }
+  })());
+});
