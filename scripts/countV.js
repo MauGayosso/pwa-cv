3703fd55288ffac895bff1c25a5a7a44114f24e7
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 <17191170703@utectulancingo.edu.mx>
2023-03-12 16:49:51 -0600

    session storage

--- scripts/countV.js
+++ scripts/countV.js
@@ -1,14 +1,11 @@
 let visitors = 0;
 
 if (localStorage.getItem('visitors')) {
-    visitors = parseInt(localStorage.getItem('visitors'));
-  } else {
-    visitors = 0;
-  }
+  visitors = parseInt(localStorage.getItem('visitors'));
+} else {
+  visitors = 0;
+}
 
-visitors++;
-
-localStorage.setItem('visitors', visitors);
-
-console.log('Número de visitantes: ' + visitors);
-
+const visitorCount = document.getElementById('visitor-count');
+visitorCount.textContent = visitors;
+    
