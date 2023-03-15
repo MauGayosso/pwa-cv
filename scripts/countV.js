d559475433b6985fae101d3f4ef3d3699faf90c4
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 <17191170703@utectulancingo.edu.mx>
2023-03-14 22:38:10 -0600

    session storage

--- scripts/countV.js
+++ scripts/countV.js
@@ -1,4 +1,15 @@
 let visitors = 0;
+const currentHour = new Date().getHours();
+
+localStorage.setItem('currentHour', currentHour);
+
+const savedHour = localStorage.getItem('currentHour');
+
+const savedHourNumber = parseInt(savedHour);
+
+console.log(savedHourNumber);
+
+
 
 if (localStorage.getItem('visitors')) {
     visitors = parseInt(localStorage.getItem('visitors'));
@@ -11,6 +22,7 @@ visitors++;
 localStorage.setItem('visitors', visitors);
 
 const visitorCount = document.getElementById('visitor-count');
-
+const visitorHour = document.getElementById('visitor-hour');
+visitorHour.textContent = savedHourNumber;
 visitorCount.textContent = visitors;
 
